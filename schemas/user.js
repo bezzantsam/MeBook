f5b705f4fbbf993d69250da078c9a96224a9e427
Sam Bezzant <bezzantsam@gmail.com>
2022-10-20 06:54:21 -0700

    Add files via upload

---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: "user",
+    title: "User",
+    type: 'document',
+    fields: [
+        {
+            name: 'useName',
+            title: 'userName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'image',
+            type: 'string'
+        },
+    ]
+}
